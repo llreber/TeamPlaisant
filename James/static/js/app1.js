94ddbcd840dd84d8ff2275c25e2fe617022e00a3
Yongjin <yjjiangphysics@gmail>
2019-01-07 17:01:55 -0600

    countries goes into table entry rightly but symbols are not produced

--- James/static/js/app1.js
+++ James/static/js/app1.js
@@ -1,5 +1,6 @@
 console.log('here')
-
+console.log('here2')
+// const sqlite3 = require('sqlite3').verbose();
 
 
 var trace1 = {
@@ -54,11 +55,18 @@ Plotly.newPlot('scatter_plotly', data);
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 
-
+  function rainbow(n) {
+    //from value to color where n is from 0 to 255
+      n = n * 240 / 255;
+      return 'hsl(' + n + ',100%,50%)';
+    }
 
 var parseTime = d3.timeParse("%Y");
 // d3.csv("./us_foreign_aid_complete.csv").then(function(AidData) {
-d3.csv("./simple_data.csv").then(function(AidData) {
+
+
+country="Afghanistan"
+d3.json(`/country/${country}`).then(function(AidData) {
 
     AidData.forEach(function(data) {
     //   data.date = parseTime(data.date);
@@ -112,25 +120,32 @@ function xScale(AidData) {
     .call(leftAxis);
  
 
-
   var circlesGroup=  chartGroup.selectAll("g.dot")
   .data(AidData)
   .enter().append('g');
 
 circlesGroup.append("circle")
 .attr("class", "dot")
-.attr("r", "15")
+.attr("r", "5")
 .attr("cx",data=>xLinearScale(data["fiscal_year"]))
 .attr("cy",data=>yLinearScale(data["constant_amount"]))
-.attr("fill", "red");
+.attr("fill", rainbow(100))
+.attr("data-legend",function(d) { return d.country_name})
 
-circlesGroup.append("text").text(function(d){
-return d.abbr;
-})
-.attr("x",data=>xLinearScale(data.fiscal_year))
-.attr("y",data=>yLinearScale(data.constant_amount))
-.attr("text-anchor", "middle")
-.attr("font-size","60%")
+// circlesGroup.append("text").text(function(d){
+// return d.abbr;
+// })
+// .attr("x",data=>xLinearScale(data.fiscal_year))
+// .attr("y",data=>yLinearScale(data.constant_amount))
+// .attr("text-anchor", "middle")
+// .attr("font-size","60%")
+
+// legend = svg.append("g")
+//   .attr("class","legend")
+//   .attr("transform","translate(50,30)")
+//   .style("font-size","12px")
+//   .call(d3.legend)
+
 
 })
 
@@ -143,14 +158,77 @@ var table1 = d3.selectAll("#selected_country_table")
               .style("width","100%")
     
 var thead=table1.append('thead')
-var tbody=table1.append('tbody');
 
-thead.append('tr')
+thead.append('th')
+.style("min-width","90px")
+  .text("symbol:");
+
+thead.append('th')
+.style("min-width","190px")
+  .text("selected contries:");
+
+var tbody=table1.append('tbody');
+Legend_label=tbody.append('tr')
+// Legend_label.append('td')
+// .append("circle")
+// .attr("class", "dot")
+// .attr("r", "5")
+// .attr("fill", "red");
+
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
+
+// Legend_label.append('td')
+// var rect = element.getBoundingClientRect();
+// console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+element=Legend_label.append('td').attr('id','circle1').text(' ss')
+ss=document.getElementById("circle1")
+
+
+
+element.append('svg').attr("height","20").attr("height","20")
+.append("circle")
+.attr("cx",getOffset(ss).left)
+.attr("cy",getOffset(ss).top)
+.attr("class", "dot")
+.attr("r", "5")
+.attr("fill", "red");
+Legend_label.append('td').text("Afghanistan")
+
+
+
+
+
+// var legend_keys = ["Austin", "New York", "San Francisco"]
+
+// var lineLegend =d3.selectAll("#selected_country_table")
+//    .append('div').attr("class","col-md-12").append('div')
+// .attr("id","legend-area")
+// .style("text-align", "center")
+// .selectAll(".Legend")
+//     .data(legend_keys)
+//     .enter().append("g")
+//     .attr("class","Legend")
+//     .attr("transform", function (d,i) {
+//             return "translate(" + svgWidth  + "," + (i*20)+")";
+//         });
+
+// lineLegend.append("text").text(function (d) {return d;})
+//     .attr("transform", "translate(15,9)"); //align texts with boxes
+
+// lineLegend.append("rect")
+//     .attr("fill", function (d, i) {return rainbow(i*20); })
+//     .attr("width", 10).attr("height", 10);
+
           // .selectAll('th').style("min-width","90px")
           // .data(columns).enter()
-          .append('th')
-          .style("min-width","90px")
-            .text("selected contries:");
+         
 
 
 MoreFilter=d3.select('body').append('div').append('center').attr("class","col-md-10").append('div').attr("class","btn-group btn-group-lg")
@@ -160,7 +238,8 @@ MoreFilter.append("button").attr("type","button")
  
 // data_3 =[Array.from(filter_data[filter_kind])]; 
 data_3=[]
-d3.csv("./country_name.csv").then(function(CountryNames) { 
+
+d3.json("./names").then(function(CountryNames) { 
   CountryNames.forEach(function(data) {
     //   data.date = parseTime(data.date);
     data_3.push(data.country);
@@ -193,7 +272,10 @@ function onchange() {
   filter_value = d3.select('select').property('value')
   console.log('Onchange:',filter_value)
 
-  tbody.append('tr').append('td').text(filter_value)
+  Legend_label=tbody.append('tr')
+  Legend_label.append('td').text("color")
+  Legend_label.append('td').text(filter_value)
+
   
   // .selectAll('th').style("min-width","90px")
   // .data(columns).enter()
